f638f341002941506ad8230b134fa37a387709a2
Cris <crisheight@gmail.com>
2024-04-14 22:16:22 -0500

    Updated config and index with the addition of MongoDB. Used mongoose to connect to the DB, changed logic so server launches if DB is connected properly.

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -8,6 +9,13 @@ app.get('/', (request, response) => {
     return response.status(234).send('Welcome to MERN stack');
 })
 
-app.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
-});
+mongoose.connect(mongoDBURL)
+        .then(() => {
+            console.log("MongoDB Connected");
+            app.listen(PORT, () => {
+                console.log("App listening to port " + PORT);
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+        });
