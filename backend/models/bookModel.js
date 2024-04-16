bf6ee9e458285553d16be75ad4e1a48b884e4d45
Cris <crisheight@gmail.com>
2024-04-15 20:30:41 -0500

    Adjusted Schema for readability.

--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -1,22 +1,23 @@
 import mongoose from "mongoose";
-const bookSchema = mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    author: {
-        type: String,
-        required: true,
-    },
-    publishYear: {
-        type: Number,
-        required: true,
 
-    }
-    },  //end first param
+const bookSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        author: {
+            type: String,
+            required: true,
+        },
+        publishYear: {
+            type: Number,
+            required: true,
+        },
+    }, //End first param
     {
         timestamps: true,
     }
-    ); //End Schema
+); //End Schema
 
-export const Book = mongoose.model('Cat', bookSchema);
+export const Book = mongoose.model('Book', bookSchema);
