1f3b8bd6aa53b2ae80c8afe2e6b81fb01f0e0072
Cris <crisheight@gmail.com>
2024-04-15 20:32:31 -0500

    Create and handle a promise that checks if its valid and either returns a 400 status with an error or adds the information into book for our bookModel with a status 201.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
 
 const app = express();
 
@@ -8,6 +9,34 @@ app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Welcome to MERN stack');
 })
+
+
+//Route for saving a new book
+app.post('/books', async (request, response) => {
+    try {
+        if(
+            !request.body ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Submit all required fields: title, author, publishYear'
+            });
+        } //End return
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        }; //End newBook
+
+        const book = await Book.create(newBook);
+        response.status(201).send(book);
+
+    } catch(error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    } //End catch
+});
 
 mongoose.connect(mongoDBURL)
         .then(() => {
