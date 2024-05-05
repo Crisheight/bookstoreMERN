42cc2babee9f81275e8e2ea1944c131f248e38e8
Cris <crisheight@gmail.com>
2024-05-05 13:03:08 -0500

    import useSnackbar, make a const var and use it to replace our alert functions so the user gets a visual notification of created book or failed action.

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -3,6 +3,7 @@ import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useSnackbar } from 'notistack';
 
 
 const CreateBooks = () => {
@@ -11,6 +12,8 @@ const CreateBooks = () => {
     const [publishYear, setPublishYear] = useState('');
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
+    const { enqueueSnackbar } = useSnackbar();
+
     const handleSaveBook = () => {
         const data = {
             title,
@@ -21,11 +24,12 @@ const CreateBooks = () => {
         axios.post('http://localhost:5555/books', data)
             .then(() => {
                 setLoading(false);
+                enqueueSnackbar('Book created successfully.', { variant: 'success' });
                 navigate('/');
             })
             .catch(error => {
                 setLoading(false);
-                alert('An error occurred.');
+                enqueueSnackbar('An error occurred.', { variant: 'error' });
                 console.log(error);
 
             });
