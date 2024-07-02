fe2c271e2558c731b10c245786c2043ca4c6fe9d
Cris <crisheight@gmail.com>
2024-07-02 17:57:45 -0500

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-    'mongodb+srv://crisheight:XIqOUZkDDHtZtrDi@book-store-mern.rfv6g46.mongodb.net/books-collection?retryWrites=true&w=majority&appName=book-store-MERN';
+    'mongodb://localhost';
